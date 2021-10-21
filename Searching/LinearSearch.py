2edbeaae53ae0fcb76886472a507271195e180ea
Tribikram Dhar <74320520+threedee003@users.noreply.github.com>
2021-10-21 23:16:54 +0530

    Add files via upload

---
+++ Searching/LinearSearch.py
@@ -0,0 +1,19 @@
+def LinearSearch(array,key):
+    flag = False
+    for i in range(0,len(array)):
+        if array[i]==key :
+            flag = True
+            print("The element is in the {} position".format(i+1))
+    if not flag:
+        print("Key not found.")
+
+
+def main():
+    array = eval(input("Enter the array : "))
+    key = eval(input("Enter the key to be found : "))
+    LinearSearch(array,key)
+
+
+
+if __name__ == '__main__':
+    main()
